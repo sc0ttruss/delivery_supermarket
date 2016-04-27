02bae05add2cdde719f19332c1ece15cb23a6b7e
Scott <sr@chef.io>
2016-04-27 21:26:48 +0300

    fix node proprties with an override in recipe, to override .kitchen.yml properties

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,4 +4,17 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
+# read the local supermareket.json file and extract
+# the oauth2 app id and secret.  This file comes from
+# chef Server
+
+config = JSON.parse(File.read('/mnt/share/chef/supermarket.json' ))
+puts "config is ", config.to_s
+uid = config['uid']
+secret = config['secret']
+node.override['supermarket_omnibus']['chef_oauth2_app_id'] = uid
+node.override['supermarket_omnibus']['chef_oauth2_secret'] = secret
+#  chef_server_url: 'https://chef.myorg.chefdemo.net'
+# puts "uid is ------ ", uid
+# puts "secret is ----- ", secret
 include_recipe 'supermarket-omnibus-cookbook::default'
